67876518f27930b9c70aaa48485492390618e4fd
BatteryDrain <115777815+BatteryDrain@users.noreply.github.com>
2025-02-13 14:10:41 -0700

    added array builder f(x)

--- java.js
+++ java.js
@@ -181,4 +181,12 @@ function setroundnumber(){
     NUMBER_OF_ROUNDS = NUMBER_OF_USERS * 3 + 12;
     numberOfPlayers.innerHTML = "there are " + NUMBER_OF_USERS + " players";
     numberOfRounds.innerHTML = "there will be " + NUMBER_OF_ROUNDS + " rounds";
+function newcsvToArray(csvString) {
+    temp = "";
+    i = 0;
+    while(i < 10 && temp != ","){
+        temp = temp + csvString[i];
+        i++;
+    }
+}
 }
