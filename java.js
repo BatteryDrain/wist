d7310b73c26630f0dc3f5a33fe97037bf67fd638
BatteryDrain <115777815+BatteryDrain@users.noreply.github.com>
2025-02-13 16:17:35 -0700

    removed if

--- java.js
+++ java.js
@@ -198,12 +198,12 @@ function newcsvToArray(csvString) {
         // || "\\"
         while(i < 1000 && csvString[i] != ","){
             temp = temp + csvString[i];
-            if(csvString[i] == ","){
-                i++;
-            }
-            else{
-                i = i + 3;
-            }
+            // if(csvString[i] == ","){
+                 i++;
+            // }
+            // else{
+            //     i = i + 3;
+            // }
         }
         console.log("ii = " + ii + " " + temp);
         if(ii % 17 == 1){
