ca1376187ed8236de034f11df84ff006c4fd7cbf
BatteryDrain <115777815+BatteryDrain@users.noreply.github.com>
2025-02-26 13:54:14 -0700

    fixed loops

--- java.js
+++ java.js
@@ -154,7 +154,7 @@ function csvToBIGARRAY(csvString) {
 }
 
 function buildTotals(){
-    for(x = TABLE_1_OFFSET_X; x < ((NUMBER_OF_USERS * 2)+TABLE_1_OFFSET_X); x++){
+    for(x = 0; x < (NUMBER_OF_USERS * 2); x++){
         for(y = 0 ; y < NUMBER_OF_ROUNDS; y++){
             if(BIGARRAY[x][y] == "" || BIGARRAY[x + 1][y] == ""){
                 TOTALSARRAY[x].push("");
