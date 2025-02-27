71d50bcdb352f2881c7dd7274f65664a46cc504a
BatteryDrain <115777815+BatteryDrain@users.noreply.github.com>
2025-02-27 05:02:40 -0700

    forgot about index 0

--- java.js
+++ java.js
@@ -197,21 +197,21 @@ function buildgraph() {
             labels: ['January', 'February', 'March', 'April', 'May', 'June'],
             datasets: [
                 {
-                    label: PLAYERNAMES[1],
+                    label: PLAYERNAMES[0],
                     data: [12, 19, 3, 5, 2, 3],
                     borderColor: 'red',
                     backgroundColor: 'rgba(255, 0, 0, 0.2)',
                     borderWidth: 2
                 },
                 {
-                    label: PLAYERNAMES[2],
+                    label: PLAYERNAMES[1],
                     data: [5, 15, 8, 12, 6, 10],
                     borderColor: 'blue',
                     backgroundColor: 'rgba(0, 0, 255, 0.2)',
                     borderWidth: 2
                 },
                 {
-                    label: PLAYERNAMES[3],
+                    label: PLAYERNAMES[2],
                     data: [5, 15, 8, 12, 6, 10],
                     borderColor: 'blue',
                     backgroundColor: 'rgba(0, 0, 255, 0.2)',
