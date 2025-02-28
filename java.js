21107c9220fe8bcd741df1f29a0c9cd932649d69
BatteryDrain <115777815+BatteryDrain@users.noreply.github.com>
2025-02-28 06:41:49 -0700

    fixed logs

--- java.js
+++ java.js
@@ -299,10 +299,11 @@ stopbutton.addEventListener("click", function(){
     console.log("clicked");
     if(STOP == true){
         STOP = false;
-        console.log("started");
+        console.log("refresh" + STOP);
     }
     else{
         STOP = true;
-        console.log("stopped");
+        console.log("refresh" + STOP);
+        secondstest();
     }
 });
