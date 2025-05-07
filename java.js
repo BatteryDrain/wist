5ed6ed1000814686dac3cc0489825e4d2dd4de9f
BatteryDrain <115777815+BatteryDrain@users.noreply.github.com>
2025-05-07 11:29:46 -0600

    fixing refresh

--- java.js
+++ java.js
@@ -308,7 +308,7 @@ stopbutton.addEventListener("click", function(){
     else{
         STOP = true;
         console.log("stopping refresh = " + STOP);
-        resetonseconds();
+        //resetonseconds();
         stopbutton.innerHTML="start";
     }
 });
